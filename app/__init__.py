d7298ac4937a88db37e4982cde1d96a44fb4ac7c
JoeyCorbett <corbettjoey9@gmail.com>
2024-11-14 09:21:28 -0500

    Move logic to __init__.py to create app factory and create starting routes
    Moved logic to  __init__.py to be more aligned with flask best practices and created login / register pages. Imported login_manager and made home only accessible to authenticated users. Setup basic routes with POST / GET request logic

---
+++ app/__init__.py
@@ -0,0 +1,66 @@
+from flask import Flask, render_template, redirect, url_for, request
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager, login_user, login_required, logout_user, current_user
+
+# Initialize extensions
+db = SQLAlchemy()
+bcrypt = Bcrypt()
+login_manager = LoginManager()
+
+def create_app():
+    app = Flask(__name__)
+    app.config['SECRET_KEY'] = 'your_secret_key_here'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///StockStudy.db'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False 
+
+    db.init_app(app)
+    bcrypt.init_app(app)
+    login_manager.init_app(app)
+
+    login_manager.login_viwe = 'login'
+
+    @login_manager.user_loader
+    def load_user(user_id):
+        return User.query.get(int(user_id))
+
+    with app.app_context():
+        from .models.user import User
+        db.create_all()
+
+    @app.route('/')
+    def index():
+        return render_template('index.html')
+    
+    @app.route('/login', methods=['GET', 'POST'])
+    def login():
+        if current_user.is_authenticated:
+            return redirect(url_for('home'))
+        
+        if request.method == "POST":
+            print("Post")
+
+        return render_template("login.html")
+    
+    @app.route('/register', methods=['GET', 'POST'])
+    def register():
+        if current_user.is_authenticated:
+            return redirect(url_for('home'))
+        
+        if request.method == "POST":
+            print("Post")
+            
+        return render_template("register.html")
+
+    @app.route('/home')
+    @login_required
+    def home():
+        return render_template("home.html")
+    
+    @app.route('/logout')
+    def logout():
+        logout_user()
+        return redirect(url_for('login'))
+
+
+    return app
