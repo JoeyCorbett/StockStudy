286d817875c99a0c140b0d1d5df871ac2b0a6079
JoeyCorbett <corbettjoey9@gmail.com>
2024-12-08 16:39:31 -0500

    feat: Implement quick view modal with group details
    - Now when finding groups, users can hit 'quick view' and a modal will appear with more group details generated dynamically with javascript

--- app/static/script.js
+++ app/static/script.js
@@ -26,3 +26,29 @@ function setRemoveAction(groupID, memberID) {
     const removeForm = document.getElementById('removeMemberForm')
     removeForm.action = `/remove-member/${groupID}/${memberID}`;
 }
+
+document.addEventListener('show.bs.modal', function (event) {
+    const modal = event.target;
+
+    if (modal.id == 'quickViewModal') {
+        const button = event.relatedTarget;
+
+        const groupId = button.getAttribute('data-group-id');
+        const groupName = button.getAttribute('data-group-name');
+        const groupSubject = button.getAttribute('data-group-subject');
+        const groupOwner = button.getAttribute('data-group-owner');
+        const groupDescription = button.getAttribute('data-group-description');
+        const groupType = button.getAttribute('data-group-type');
+        const groupMembers = button.getAttribute('data-group-members');
+        const groupLocation = button.getAttribute('data-group-location');
+
+
+        modal.querySelector('.modal-title').textContent = groupName;
+        modal.querySelector('.modal-subject').textContent = groupSubject;
+        modal.querySelector('.modal-owner').textContent = groupOwner;
+        modal.querySelector('.modal-description').textContent = groupDescription;
+        modal.querySelector('.modal-type').textContent = groupType;
+        modal.querySelector('#modal-members-list').textContent = groupMembers;
+        modal.querySelector('.modal-location').textContent = groupLocation;
+    }
+});
