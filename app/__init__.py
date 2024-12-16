dbc1787efe668ca1f4b7758f815337fd2193e7d1
JoeyCorbett <corbettjoey9@gmail.com>
2024-12-16 05:34:39 -0500

    fix: Update database URL handling for PostgreSQL compatibility

--- app/__init__.py
+++ app/__init__.py
@@ -16,7 +16,11 @@ load_dotenv()
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
-    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL', 'sqlite:///StockStudy.db')
+    db_url = os.getenv('DATABASE_URL', 'sqlite:///StockStudy.db')
+    if db_url.startswith("postgres://"):
+        db_url = db_url.replace("postgres://", "postgresql://", 1)
+
+    app.config['SQLALCHEMY_DATABASE_URI'] = db_url
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
   #  app.config['PERMANENT_SESSION_LIFETIME'] = timedelta(minutes=10)
 
